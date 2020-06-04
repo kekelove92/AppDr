20df7d32c8bd2f8c4081896f6e8e7f3ccf47fde8
tunganh3011 <tunganh3011@gmail.com>
2020-06-04 17:41:23 +0700

    first commit

---
+++ app/src/main/java/com/newapp1/appdr/LoginActivity.java
@@ -0,0 +1,39 @@
+package com.newapp1.appdr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.edt_email)
+    EditText edtEmail;
+    @BindView(R.id.edt_password)
+    EditText edtPw;
+    @BindView(R.id.btn_login)
+    Button btnLogin;
+    @BindView(R.id.cb)
+    CheckBox checkBox;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        btnLogin.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
